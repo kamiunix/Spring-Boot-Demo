9af364c7961faab340a79c3fd181cdab5d561f56
kamiunix <sam.champagne@dal.ca>
2020-09-18 19:57:08 -0300

    Initial Commit

---
+++ src/main/java/com/example/demo/Controller/StudentController.java
@@ -0,0 +1,41 @@
+package com.example.demo.Controller;
+
+import com.example.demo.Entity.Student;
+import com.example.demo.Service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+    @Autowired
+    private StudentService studentService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public Collection<Student> getAllStudents(){
+        return studentService.getAllStudents();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Student getStudentById(@PathVariable("id") int id){
+        return this.studentService.getStudentById(id);
+    }
+
+    @RequestMapping(value="/{id}", method = RequestMethod.DELETE)
+    public void  deleteStudentById(@PathVariable("id") int id){
+        this.studentService.removeStudentById(id);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void updateStudent(@RequestBody Student student){
+        this.studentService.updateStudent(student);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void addStudent(@RequestBody Student student){
+        this.studentService.addStudent(student);
+    }
+}
---
+++ src/main/java/com/example/demo/Dao/StudentDao.java
@@ -0,0 +1,46 @@
+package com.example.demo.Dao;
+
+import com.example.demo.Entity.Student;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Repository
+public class StudentDao {
+
+    private static Map<Integer, Student> students;
+
+    static {
+        students = new HashMap<Integer, Student>(){
+            {
+                put(1, new Student(1, "Sam", "Computer Science"));
+                put(2, new Student(2, "Padreac", "Computer Science"));
+                put(3, new Student(3, "Sandra", "Dietetics"));            }
+        };
+    }
+
+    public Collection<Student> getAllStudents(){
+        return this.students.values();
+    }
+
+    public Student getStudentById(int id){
+        return this.students.get(id);
+    }
+
+    public void removeStudentById(int id) {
+        this.students.remove(id);
+    }
+
+    public void updateStudent(Student student) {
+        Student s = students.get(student.getId());
+        s.setCourse(student.getCourse());
+        s.setName(student.getName());
+        students.put(student.getId(), s);
+    }
+
+    public void addStudent(Student student) {
+        students.put(student.getId(), student);
+    }
+}
